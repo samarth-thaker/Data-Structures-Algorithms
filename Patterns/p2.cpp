d8568645f4d4859c0569384c961ca1e9890ad4d2
samarth-thaker <samarth.thaker07@gmail.com>
2024-01-18 23:00:55 +0530

    Patterns

---
+++ Patterns/p2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=-0; i<5; i++){
+        for(int j=4; j>i; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
